280c55bf6d7a2a8123a5a5b81e104cbd3a4ece5f
丨Rainbow丨 <775183940@qq.com>
2019-11-04 13:31:02 +0800

    Hikvision 8700调试完成 可以使用

--- app/src/main/java/com/app/hikvision8700/MainActivity.java
+++ app/src/main/java/com/app/hikvision8700/MainActivity.java
@@ -1,5 +1,8 @@
 package com.app.hikvision8700;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String ip = edtIp.getText().toString().trim();
                 String userName = edtUserName.getText().toString().trim();
                 String passWord = edtPassword.getText().toString().trim();
@@ -35,5 +39,20 @@ public class MainActivity extends AppCompatActivity {
         edtUserName = findViewById(R.id.main_edt_username);
         edtPassword = findViewById(R.id.main_edt_pass);
         btnLogin = findViewById(R.id.main_btn_login);
+
+        edtIp.setText(getPlaceHolder("ip") + getPlaceHolder("port"));
+        edtUserName.setText(getPlaceHolder("name"));
+        edtPassword.setText(getPlaceHolder("password"));
     }
+
+
+    public String getPlaceHolder(String key) {
+        try {
+            ApplicationInfo appInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString(key);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
 }
